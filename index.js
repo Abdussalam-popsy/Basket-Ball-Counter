9e0992d3fc88958e27874879e62da7fd91c4daa3
Abdussalam-popsy <69190355+Abdussalam-popsy@users.noreply.github.com>
2025-07-20 15:12:13 +0100

    Publish
    From a scrim to git

---
+++ index.js
@@ -0,0 +1,44 @@
+let count = 0;
+const homeCount = document.querySelector(".home-count");
+const guestCount = document.querySelector(".guest-count");
+
+let countOne = document.querySelector("add-one");
+let countTwo = document.querySelector("add-two");
+let countThree = document.querySelector("add-three");
+
+function addOne() {
+    count += 1
+    homeCount.textContent = count
+}
+
+function addTwo() {
+    count += 2
+    homeCount.textContent = count
+}
+
+function addThree() {
+    count += 3
+    homeCount.textContent = count
+}
+
+
+function gOne() {
+    count += 1
+    guestCount.textContent = count
+}
+
+function gTwo() {
+    count += 2
+    guestCount.textContent = count
+}
+
+function gThree() {
+    count += 3
+    guestCount.textContent = count
+}
+
+function reset() {
+    count = 0
+    homeCount.textContent = 0
+    guestCount.textContent = 0
+}
